b9997fd0d5da1d65981f821451c2d780bc68b6c3
ALiGoo <coh4ry7z@gmail.com>
2021-06-04 10:55:02 +0800

    update running script

--- run.sh
+++ run.sh
@@ -2,3 +2,8 @@ wget -P raw_data https://gbfs.citibikenyc.com/gbfs/en/station_information.json
 cat raw_data_urls.txt | xargs -n 1 -P 6 wget -P raw_data/
 unzip 'raw_data/*.zip' -d raw_data/
 mkdir process_data
+python process_raw_data.py
+python featurize.py
+python generate_dataloader.py
+python training.py
+python evaluation.py
